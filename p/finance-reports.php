a5e2a6cd5d27bed3a6a2357fbea8a8e2f29b49ac
Greg <gregzzmail@gmail.com>
2018-11-07 15:10:50 +0200

    Generate finance report list programatically

--- p/finance-reports.php
+++ p/finance-reports.php
@@ -4,11 +4,11 @@ include_start_html("Finance Reports");
 include ($_SERVER['DOCUMENT_ROOT'].'/php/html/header.php');
 
 $conn = db_conn_read_only();
+
 $sql = "SELECT sum(amount_c)/100 FROM savings WHERE type=\"travel\"";
 $balance_travel = array_values(mysqli_fetch_assoc(mysqli_query($conn, $sql)))[0];
 $sql = "SELECT sum(amount_c)/100 FROM savings WHERE type=\"equipment\"";
 $balance_equipment = array_values(mysqli_fetch_assoc(mysqli_query($conn, $sql)))[0];
-$conn->close();
 ?>
 
 <div id="page-wrapper">
@@ -33,51 +33,26 @@ $conn->close();
 
     <h2>Detailed Monthly Reports</h2>
     <ul>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1yc81mjhghmpzsx3C4qf2MF0aRhXwJNK7uOZl3H3JncY/edit?usp=sharing">September 2018</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1oLm5tZhfDsxyrGOQvJ4xqW2FUlCbzpzKccx1AmYd31M/edit?usp=sharing">August 2018</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/16wib6zEofhsCCtC3k0MvBfIfjcL9HtBfsVGj5qyoj3Q/edit?usp=sharing">July 2018</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1sloxQDB9bYtJKtaeXLCbY5WjogvC-Y0dimEvqa_iePI/edit?usp=sharing">June 2018</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1O_0erYD3EofQf55MwKQA_1YqslP9TvBLAG0btAyuZzI/edit?usp=sharing">May 2018</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1_hO7dUWh_HjbZMR40ye9ogXKT6OozsWKnt0N2WAyVfc/edit?usp=sharing">April 2018</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1sHzHxoHzUo_orhHE0Ro36WzbwQvsRW84TOgGJ-Gsr90/edit?usp=sharing">March 2018</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1pa5dQbNAhxknCkECFBZLyIVuFtRyrIipdgApx-YsfRo/edit?usp=sharing">February 2018</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1e676qn5VG6Y7nZPI4UBulAvBjmhjTsfIvbszVnvteng/edit?usp=sharing">January 2018</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/17ps67HlCubmyR_8uq4j8xs4lZ6INT6e-t6B0Ht-rdpk/edit?usp=sharing">December 2017</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1FM0ajmBFhHn7JcHlfpxBS0ASNz4cdyec5PcxVS2EJyE/edit?usp=sharing">November 2017</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/spreadsheets/d/1KYJdZfVw-ZLUBy8dqFheK4FI7TlJG31ChL2U4DFGKM0/edit?usp=sharing">October 2017</a> - More info and stats about the first month <a href="https://blog.hdrihaven.com/2017/11/20/an-unbelievable-first-month-stats-reports-etc/">here</a>.
-        </li>
+    <?php 
+    $sql = "SELECT * FROM `finance_reports` ORDER BY `datetime` desc";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $link = $row['link'];
+            $month = date("F Y", strtotime($row['datetime']));
+            echo "<li>";
+            echo "<a href=\"{$link}\">{$month}</a>";
+            echo "</li>";
+        }
+    }
+    ?>
     </ul>
 
     <p>
         For a quick visual overview of how HDRI Haven has grown, I recommend checking out the <a href="https://graphtreon.com/creator/hdrihaven">Graphtreon page</a>.
     </p>
 
-    <?php 
-    $conn = db_conn_read_only();
-
+    <?php
     $sql = "SELECT * FROM `savings` WHERE type = \"travel\" ORDER BY `datetime` desc";
     $result = mysqli_query($conn, $sql);
     $travel = array();
@@ -95,8 +70,6 @@ $conn->close();
             $equipment[$row['id']] = $row;
         }
     }
-
-    $conn->close();
     ?>
 
     <div class='col-2' style='vertical-align: top'>
@@ -169,6 +142,7 @@ $conn->close();
 </div>
 
 <?php
+$conn->close();
 include ($_SERVER['DOCUMENT_ROOT'].'/php/html/footer.php');
 include ($_SERVER['DOCUMENT_ROOT'].'/php/html/end_html.php');
 ?>
