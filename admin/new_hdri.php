c962ceae80064a2d7b0fd9d89d601c672578a4ee
gregzaal <gregzzmail@gmail.com>
2019-09-03 13:55:08 +0200

    add_hdri tweaks

--- admin/new_hdri.php
+++ admin/new_hdri.php
@@ -24,10 +24,6 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/functions.php');
 <div class="col-2">
 
 <form action="/admin/new_hdri_submit.php" method="GET" id="new-hdri-form">
-    <!-- [Calc EVs]
-    [Fetch file sizes] -->
-
-    
     <div class="form-item">
     <h2>Name:</h2>
     <input id="form-name" type="text" name="name" value="">
@@ -95,6 +91,9 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/functions.php');
                 echo "<div class='button cat-option'>";
                 echo $cat;
                 echo "</div>";
+                if (in_array($cat, ["studio", "urban", "midday", "overcast", "low contrast"])){
+                    echo "<br>";
+                }
             }
         }
     }
@@ -102,7 +101,7 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/functions.php');
     ?>
     </div>
     </div>
-    
+
     <div class="form-item">
     <h2>Tags:</h2>
     <input id="form-tags" type="text" name="tags" value="">
@@ -129,9 +128,9 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/functions.php');
         if ($tag){
             $freq = $all_tags[$tag];
             echo "<div class='button tag-option' style='opacity:";
-            echo pow(($freq/7), 1)+0.4;
+            echo ($freq/10) + 0.4;
             echo ";font-size:";
-            echo min(100, map_range($freq, 1, 3, 75, 100));
+            echo min(100, map_range($freq, 1, 10, 60, 90));
             echo "%'>";
             echo $tag;//." ".$freq;
             echo "</div>";
@@ -141,45 +140,45 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/functions.php');
     ?>
     </div>
     </div>
-    
+
     <div class="form-item">
     <h2>When to publish:</h2>
     <input id="form-date-published" type="text" name="date_published" value="Immediately">
     <i class="fa fa-question-circle show-tooltip" aria-hidden="true"></i><br>
     <div class="tooltip hidden">The date and time (24h format) when this product should be published, in the format: <q>YYYY/MM/DD HH:MM:SS</q>.<br>(e.g. <q>2017/05/22 17:59</q>, or just <q>2017/05/22</q> which will publish at midnight).</div>
     </div>
-    
+
     <div class="form-item">
     <h2>Backplates:</h2>
     <input id="form-backplates" type="checkbox" name="backplates" value="">
     </div>
-    
+
     <div class="form-item">
     <h2>Problem:</h2>
     <input id="form-problem" type="text" name="problem" value="">
     <i class="fa fa-question-circle show-tooltip" aria-hidden="true"></i>
     <div class="tooltip hidden">Any issue with the HDRI, e.g. clipped, low res...<br>This is the actual text displayed on the site.</div>
     </div>
-    
+
     <div class="form-item">
     <h2>Author:</h2>
     <input id="form-author" type="text" name="author" value="Greg Zaal">
     <i class="fa fa-question-circle show-tooltip" aria-hidden="true"></i>
     <div class="tooltip hidden">The original creator of this HDRI. Credit is shown on the HDRI page.</div>
     </div>
-    
+
     <div class="form-item">
     <h2>File Format:</h2>
     <input id="form-fileformat" type="text" name="fileformat" value="hdr">
     <i class="fa fa-question-circle show-tooltip" aria-hidden="true"></i>
     <div class="tooltip hidden">hdr, exr, etc...</div>
     </div>
-    
+
     <div class="form-item">
     <h2>Facebook/Twitter:</h2>
     <input id="form-twitface" type="text" name="twitface" value="New ##category## HDRI by ##author## - ##name##: ##link## #b3d #free #hdri #cc0">
     </div>
-    
+
     <div class="form-item">
     <h2>Reddit:</h2>
     <input id="form-reddit" type="text" name="reddit" value="##category## HDRI: ##name##">
