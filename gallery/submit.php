cc5a67d9661337f144064c53229f1cd413d12d8c
gregzaal <gregzzmail@gmail.com>
2020-04-03 14:47:50 +0200

    Wording on gallery page, email now required

--- gallery/submit.php
+++ gallery/submit.php
@@ -11,18 +11,35 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/html/header.php');
 
     <h2>Rules:</h2>
     <ol>
-        <li>The artwork must be your own creation, and you must be allowed to display it publically.</li>
+        <li>The artwork must be <b>your own creation</b>, and you must be allowed to display it publically.</li>
         <li>You must have used one of the HDRIs from this site in the creation of your image.</li>
         <li>Nudity or other NSFW content will not be accepted.</li>
     </ol>
 
     <h2>Note:</h2>
     <p>
-        To prevent the gallery from becoming cluttered, unfortunately only the highest quality work will be accepted.
+    We get <b>a lot</b> of submissions, so unfortunately we can only accept the highest quality artwork to prevent the gallery from becoming cluttered.
+    </p>
+    <p>
+        If you would like to receive critique for your render before submitting it, you can ask for it on our Discord server:<br><a href="https://discord.gg/Dms7Mrs" target="_blank">https://discord.gg/Dms7Mrs</a>
     </p>
     <p>
         If accepted, you will be notified by the email address you provide, and your image will be displayed on the main gallery page for a minimum of three weeks. If your work is exceptional, it may remain permanently.
     </p>
+    <p>
+        At the moment we don't have a system to notify you if your artwork was rejected, so if you do not receive a response with a day or two, unfortunately we could not accept it.
+    </p>
+    <p>
+        The most common reasons for rejection are:
+    </p>
+    <ul>
+        <li>Low-effort renders (e.g. simple lighting tests).</li>
+        <li>Low quality artwork (see note above about receiving critique).</li>
+        <li>No Haven HDRI was used.</li>
+        <li>Very similar render from another artist already present (e.g. "car on backplate" type renders with the same backplate).</li>
+        <li>Multiple similar renders of the same project (e.g. different camera angles) - only submit your best render.</li>
+        <li>Too many submissions - typically we accept at most 3 images per artist per month.</li>
+    </ul>
 
     <form action="do_submit.php" method="post" enctype="multipart/form-data" id="gallery-form">
         <?php
@@ -53,8 +70,8 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/html/header.php');
 
         <div class="form-item">
             <h2>Your email:</h2>
-            <input type="text" name="author-email" placeholder="george@starwars.com" value="">
-            <p><em>(optional)</em><br>Used to notify you when your artwork is published. Stored securely and not shared with anyone.</p>
+            <input type="text" name="author-email" placeholder="george@starwars.com" value="" required>
+            <p><br>Used only to notify you when your artwork is published. Stored securely and not shared with anyone.</p>
         </div>
 
         <div class="form-item">
