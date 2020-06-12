a4c146fb59e84a1f2651f5d7e0e887f29f8b65bd
Greg Zaal <gregzzmail@gmail.com>
2020-06-12 15:02:22 +0200

    Add missing file

---
+++ php/html/ads/Grid Top.php
@@ -0,0 +1,12 @@
+<div class="adsense-unit" id="ads-grid-top">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Grid Top -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-2284751191864068"
+     <?php if($GLOBALS['WORKING_LOCALLY']){echo "data-adtest=\"on\"";} ?>
+     data-ad-slot="2629641655"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
