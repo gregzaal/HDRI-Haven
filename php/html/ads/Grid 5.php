2f048804798e613a85b6a6c55845a57f14de0323
Greg Zaal <gregzzmail@gmail.com>
2020-06-12 15:04:41 +0200

    Add ads: Grid 5&6

---
+++ php/html/ads/Grid 5.php
@@ -0,0 +1,10 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Grid 5 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:336px;height:280px"
+     data-ad-client="ca-pub-2284751191864068"
+     <?php if($GLOBALS['WORKING_LOCALLY']){echo "data-adtest=\"on\"";} ?>
+     data-ad-slot="6780639657"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
---
+++ php/html/ads/Grid 6.php
@@ -0,0 +1,10 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Grid 6 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:336px;height:280px"
+     data-ad-client="ca-pub-2284751191864068"
+     <?php if($GLOBALS['WORKING_LOCALLY']){echo "data-adtest=\"on\"";} ?>
+     data-ad-slot="4920763078"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
