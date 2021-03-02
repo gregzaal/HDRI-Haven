459f7b782b00559ba5bee47897cc48b43dcc063a
Greg Zaal <gregzzmail@gmail.com>
2021-03-02 13:48:29 +0200

    Add Revive info to privacy policy, remove adsense & disqus info

--- p/privacy.php
+++ p/privacy.php
@@ -9,24 +9,22 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/html/header.php');
 
     <h2>Third Party Cookies</h2>
     <p>
-        We use some third party services (Google Analytics, Google AdSense, and Disqus) which store cookies on your device that are necessary for using their services, and track data about you for their own purposes. You can read more about what data they store and how they use it on their own Privacy Policies:
+        We use some third party services (Google Analytics, Revive Adserver) which store cookies on your device that are necessary for using their services, and track data about you for their own purposes. You can read more about what data they store and how they use it on their own Privacy Policies:
     </p>
     <ul>
-        <li><a href="https://policies.google.com/technologies/partner-sites">Google</a> (site analytics & ads)</li>
-        <li><a href="https://help.disqus.com/en/articles/1717103-disqus-privacy-policy">Disqus</a> (comment system)</li>
+        <li><a href="https://policies.google.com/technologies/partner-sites">Google</a> (site analytics)</li>
+        <li><a href="https://www.revive-adserver.net/privacy/">Revive</a> (banner ads)</li>
     </ul>
     <p>
-    Third party vendors, including Google, use cookies to serve ads based on your prior visits to our website or other websites. Google's use of advertising cookies enables it and its partners to serve ads to you based on your visit to our site and/or other sites on the Internet.
-    <p>
-        If you are not comfortable with this, you can opt out of personalized advertising in your <a href="https://www.google.com/settings/ads">Ads Settings</a>, or use an ad-blocker such as <a href="https://github.com/gorhill/uBlock">uBlock Origin</a>.
+        If you are not comfortable with this, we recommend you use a trusted open source ad-blocker such as <a href="https://github.com/gorhill/uBlock">uBlock Origin</a>.
     </p>
 
     <h2>IP Logging</h2>
     <p>
-        This server logs a hashed version of your IP address when you download an HDRI.
+        This server logs a hashed version of your IP address when you download an asset.
     </p>
     <p>
-        This helps us to see which HDRIs are downloaded by the most users instead of only a total download count. Doing this allows us to see what is popular and make decisions about what to shoot next.
+        This helps us to see which assets are downloaded by the most users instead of only a total download count. Doing this allows us to see what is popular and make decisions about what to shoot next.
     </p>
     <p>
         If you're not comfortable with this, you can use a VPN to obscure your public IP address. If you wish for us to delete all records downloads from your IP address, you can <?php insert_email("contact us") ?>.
@@ -43,7 +41,7 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/html/header.php');
     <hr>
 
     <p>
-        Other than what's mentioned above, HDRI Haven does not store any user data or cookies.
+        Other than what's mentioned above, we do not store any user data or cookies.
     </p>
     <p>
         If you have any other questions or concerns, please contact me: <?php insert_email() ?>
